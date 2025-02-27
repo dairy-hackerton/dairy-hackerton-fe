f8420819e30b12250f4800d2838dafeba30141cc
Yunju07 <juile0827@naver.com>
2025-02-27 17:49:31 +0900

    style: UI 디자인 개선 및 css 수정

--- dairy-fe/src/pages/Home.js
+++ dairy-fe/src/pages/Home.js
@@ -6,7 +6,12 @@ import Calendar from "../components/Calendar";
 function Home() {  
   return (
   <div>
-    <h1 className="title">📝 dAIry</h1>
+    <div class="title-container">
+      <div className="title-wrapper">
+        <h1 class="title">📝 dAIry</h1>
+        <p className="summary">AI로 간편하게 기록하는 당신의 하루</p>
+      </div>  
+    </div>
     <Calendar />
   </div>
   );
